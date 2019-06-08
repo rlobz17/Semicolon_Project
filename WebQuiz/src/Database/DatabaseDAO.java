fe638c4ec5ef7bee88e4bd582424a4171c058564
rlobz17 <rlobz17@freeuni.edu.ge>
2019-06-09 03:00:49 +0400

    Database Dao added (part2)

--- WebQuiz/src/Database/DatabaseDAO.java
+++ WebQuiz/src/Database/DatabaseDAO.java
@@ -1,5 +1,16 @@
 package Database;
 
-public class DatabaseDAO {
+import java.sql.Statement;
 
+public class DatabaseDAO {
+	 
+	private Statement stm;
+	
+ 
+	public DatabaseDAO(Statement statement) {
+		stm = statement;
+	}
+	
+	
+		
 }
