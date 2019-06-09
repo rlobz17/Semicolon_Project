60d9baf2a9ad8514e1b184b69b57e1137b499bfd
Shota <snoza17@freeuni.edu.ge>
2019-06-09 22:01:19 +0400

    accmanager, userdb

---
+++ WebQuiz/src/users/AccountManager.java
@@ -0,0 +1,19 @@
+package users;
+
+public class AccountManager {
+	public AccountManager() {
+		
+	}
+	
+	public void addNewAccount() {
+		
+	}
+	
+	public boolean containsAccount(String username) {
+		return false;
+	}
+	
+	public boolean doLogin(String username, String password) {
+		return true;
+	}
+}
---
+++ WebQuiz/src/users/UserDB.java
@@ -0,0 +1,41 @@
+package users;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import javax.swing.table.AbstractTableModel;
+
+import com.mysql.jdbc.Driver;
+
+import Database.DataBaseINFO;
+
+public class UserDB {
+	public static void addUser(User user) {
+		try{  
+			// Build Path -> add extrenal archives -> "mysql-connector-java-8.0.16.jar"
+			Class.forName("com.mysql.jdbc.Driver");
+
+			Connection con = DriverManager.getConnection("jdbc:mysql://"
+					+ DataBaseINFO.MYSQL_DATABASE_SERVER, DataBaseINFO.MYSQL_USERNAME, DataBaseINFO.MYSQL_PASSWORD);
+
+			Statement stmt = con.createStatement();
+			stmt.executeQuery("USE " + DataBaseINFO.MYSQL_DATABASE_NAME);
+			
+			String insert = "INSERT INTO accounts (account_first_name, account_last_name, account_username, account_mail, account_password) VALUES (";
+			
+			stmt.executeUpdate(insert);
+
+			con.close();  
+		}catch(Exception e){
+			System.out.println(e);
+		}
+	}
+	
+	public static boolean searchUser(String username, String mail) {
+		return true;
+	}
+}
