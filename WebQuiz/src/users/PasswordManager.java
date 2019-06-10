916095f0af57593ff0f5054526572f9b82cc9b9d
dudu <dpopk17@freeuni.edu.ge>
2019-06-10 23:25:36 +0400

    Password Manager Added, User Updated

---
+++ WebQuiz/src/users/PasswordManager.java
@@ -0,0 +1,37 @@
+package users;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class PasswordManager {
+	// hashing algotithm
+	private final String hashingAlgorithm = "SHA-2";
+	
+	/**
+	 * returns hash of given password with pre defined hashing algotihm
+	 * 
+	 * */
+	
+	public String hashPassword(String password) throws NoSuchAlgorithmException {
+		MessageDigest messDig = MessageDigest.getInstance(hashingAlgorithm);
+		messDig.update(password.getBytes());
+		return hexToString(messDig.digest());
+	}
+	
+	
+	/*
+	 Given a byte[] array, produces a hex String,
+	 such as "234a6f". with 2 chars for each byte in the array.
+	*/
+	private String hexToString(byte[] bytes) {
+		StringBuffer bf = new StringBuffer();
+		for (int i=0; i<bytes.length; i++) {
+			int value = bytes[i];
+			value = value & 0xff;  
+			if (value<16) bf.append('0');
+			bf.append(Integer.toString(value, 16));
+		}
+		return bf.toString();
+	}
+	
+}
