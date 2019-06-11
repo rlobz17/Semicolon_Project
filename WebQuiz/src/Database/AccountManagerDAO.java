2185905e117412abfec585403e08ac5114a6d596
rlobz17 <rlobz17@freeuni.edu.ge>
2019-06-11 20:12:39 +0400

    Added Test for Account Manager (everything working so far)

--- WebQuiz/src/Database/AccountManagerDAO.java
+++ WebQuiz/src/Database/AccountManagerDAO.java
@@ -1,5 +1,35 @@
 package Database;
 
-public class AccountManagerDAO {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
+
+public class AccountManagerDAO {
+	
+	private Statement stm;
+	private String database;
+	
+	public AccountManagerDAO(Statement stm, String database) {
+		this.stm = stm;
+		this.database = database;
+	}
+	
+	
+	public ArrayList<String> listOfAccounts() {
+		
+		ArrayList <String> result = new ArrayList<String>();
+		try {
+			stm.executeQuery("USE "+database);
+			ResultSet rs = stm.executeQuery("SELECT e.account_username FROM accounts e");
+			while(rs.next()) {
+				result.add(rs.getString(1));
+			}	
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}	
+		
+		return result;
+	}
 }
---
+++ WebQuiz/src/Database/TestDAO.java
@@ -0,0 +1,49 @@
+package Database;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import Database.*;
+
+class TestDAO {
+	
+	private String database = DataBaseINFO.MYSQL_DATABASE_NAME;
+	private String account = DataBaseINFO.MYSQL_USERNAME;
+	private String password = DataBaseINFO.MYSQL_PASSWORD;
+	private String server = DataBaseINFO.MYSQL_DATABASE_SERVER;
+	
+	public Statement createStatement() {		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			java.sql.Connection con = DriverManager.getConnection("jdbc:mysql://"+server+"?autoReconnect=true&useSSL=false", account, password);
+			Statement stmt = con.createStatement();
+			return stmt;
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+		
+	}
+	
+	
+
+	@Test
+	void testAccountManager() {		
+		AccountManagerDAO manager = new AccountManagerDAO(createStatement(), database);
+		ArrayList<String> list = manager.listOfAccounts();
+		for(int i=0; i<list.size(); i++) {
+			System.out.println(list.get(i));
+		}
+	}
+
+}
