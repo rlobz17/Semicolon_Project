38a9d99df53a3cc1b74588ecdef675c2d676fccc
dudu <dpopk17@freeuni.edu.ge>
2019-06-11 22:42:13 +0400

    ტემპ

--- WebQuiz/src/Temp/Answer.java
+++ WebQuiz/src/Temp/Answer.java
@@ -1,5 +1,50 @@
 package Temp;
 
-public class Answer {
 
+public class Answer {
+	private int answerID;
+	private int questionID;
+	private int answerIndex;
+	private String answerDetail;
+	
+	public Answer(int answerID, int questionID,int answerIndex, String answerDetail) {
+		this.answerDetail = answerDetail;
+		this.answerID = answerID;
+		this.answerIndex = answerIndex;
+		this.answerDetail = answerDetail;
+	}
+	
+	/**
+	 * @return the answerID
+	 */
+	public int getAnswerID() {
+		return answerID;
+	}
+
+
+	/**
+	 * @return the questionID
+	 */
+	public int getQuestionID() {
+		return questionID;
+	}
+
+
+	/**
+	 * @return the answerIndex
+	 */
+	public int getAnswerIndex() {
+		return answerIndex;
+	}
+
+
+	/**
+	 * @return the answerDetail
+	 */
+	public String getAnswerDetail() {
+		return answerDetail;
+	}
+
+
+	
 }
--- WebQuiz/src/Temp/Quiz.java
+++ WebQuiz/src/Temp/Quiz.java
@@ -1,5 +1,97 @@
 package Temp;
 
+
+import java.sql.Date;
+import java.util.ArrayList;
+
+import account.Account;
+
 public class Quiz {
+	private int quizID;
+	private Account Creator;
+	private Date CreationDate;
+	private Date UpdatedDate;
+	private int quizTaken; // how many tames this quiz was ....
+	private String imgUrl;
+	private ArrayList<Question> questions;
+	
+	public Quiz(int quizID,ArrayList<Question> questions,Account Creator,Date UpdatedDate,Date CreationData,int quizTaken,String imgUrl) {
+		this.quizID = quizID;
+		this.UpdatedDate = UpdatedDate;
+		this.Creator = Creator;
+		this.CreationDate = CreationData;
+		this.quizTaken = quizTaken;
+		this.imgUrl = imgUrl;
+		this.questions = questions;
+	}
+
+	/**
+	 * @return the imgUrl
+	 */
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+
+
+	/**
+	 * @return the quitTaken
+	 */
+	public int getQuitTaken() {
+		return quizTaken;
+	}
+
+
+
+	/**
+	 * @return the creationData
+	 */
+	public Date getCreationData() {
+		return CreationDate;
+	}
+
+
+
+	/**
+	 * @return the creator
+	 */
+	public Account getCreator() {
+		return Creator;
+	}
+
+
+	/**
+	 * @param Question
+	 * added this Question to current Quize
+	 * */
+	
+	public void addQuestion(Question q) {
+		this.questions.add(q);
+	}
+	
+	
+	/**
+	 * @return the question
+	 */
+	public ArrayList<Question> getQuestions() {
+		return questions;
+	}
+
+
+
+	/**
+	 * @return the quizID
+	 */
+	public int getQuizID() {
+		return quizID;
+	}
+
+
+	/**
+	 * @return the updatedDate
+	 */
+	public Date getUpdatedDate() {
+		return UpdatedDate;
+	}
 
 }
