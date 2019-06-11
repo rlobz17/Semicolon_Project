b20d2029c248ce5fa744ed326c3523ced777d4ec
rlobz17 <rlobz17@freeuni.edu.ge>
2019-06-12 00:02:13 +0400

    Methods addNewAccount and containsAccount Implemented

--- WebQuiz/src/Temp/AccountManager.java
+++ WebQuiz/src/Temp/AccountManager.java
@@ -4,14 +4,18 @@ import java.sql.Statement;
 
 
 public class AccountManager {
-	public AccountManager() {}
+	AccountManagerDao dao; 
+	
+	public AccountManager() {
+		dao = new AccountManagerDao();
+	}
 	
 	public void addNewAccount(String firstname, String lastname, String username, String password, String mail,Statement stm) {
-		
+		dao.addNewAccount(firstname, lastname, username, password, mail, stm);
 	}
 	
 	public int containsAccount(String username, String mail, Statement stm) {
-		return 0;
+		return dao.searchUser(username, mail, stm);
 	}
 	
 	public boolean doLogin(String username, String password, Statement stm) {
--- WebQuiz/src/Temp/AccountManagerDao.java
+++ WebQuiz/src/Temp/AccountManagerDao.java
@@ -23,5 +23,59 @@ public class AccountManagerDao {
 		
 		return result;
 	}
+	
+	public void addNewAccount(String firstname, String lastname, String username, String password, String mail,Statement stm) {
+		
+		try {
+			stm.executeQuery("USE "+DataBaseINFO.MYSQL_DATABASE_NAME);
+			String insert = "INSERT INTO accounts (account_first_name, account_last_name, account_username, account_mail, account_password) VALUES (";
+			insert += "\"" + firstname + "\", ";
+			insert += "\"" + lastname + "\", ";
+			insert += "\"" + username + "\", ";
+			insert += "\"" + mail + "\", ";
+			insert += "\"" + password + "\");";
+						
+			stm.executeUpdate(insert);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}	
+	}
+	
+	
+	/**
+	 * @return 1 - if username is in use, 2 - if mail is in use, 
+	 * 3 - if username and mail both in use, 0 - otherwise
+	 */
+	public int searchUser(String username, String mail, Statement stm) {
+		int result = 0;
+		try{  
+			
+			stm.executeQuery("USE " + DataBaseINFO.MYSQL_DATABASE_NAME);
+			
+			String selectUsername = "Select * from accounts ";
+			selectUsername += "where account_username = \"" + username + "\";";
+						
+			ResultSet rs = stm.executeQuery(selectUsername);
+						
+			if(rs.next()) {
+				result = 1;
+			}
+						
+			stm.executeQuery("USE " + DataBaseINFO.MYSQL_DATABASE_NAME);
+			String selectMail = "Select * from accounts ";
+			selectMail += "where account_mail = \"" + mail + "\";";
+			
+			rs = stm.executeQuery(selectMail);
+			if(rs.next()) {
+				if(result==1) {
+					result = 3;
+				} else result = 2;
+			}
 
+		}catch(Exception e){
+			System.out.println(e);
+		}
+		return result;
+	}
+	
 }
