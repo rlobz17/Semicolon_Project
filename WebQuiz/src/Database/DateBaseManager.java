5c18ec84a9a87b427d64d98b368527b018d9ef2c
dudu <dpopk17@freeuni.edu.ge>
2019-06-11 23:23:03 +0400

    DateBaseManager added

--- WebQuiz/src/Database/DateBaseManager.java
+++ WebQuiz/src/Database/DateBaseManager.java
@@ -1,5 +1,29 @@
 package Database;
 
-public class DateBaseManager {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+public class DateBaseManager {
+	private Connection con;
+	public DateBaseManager() throws ClassNotFoundException, SQLException {
+		// Build Path -> add extrenal archives -> "mysql-connector-java-8.0.16.jar"
+		Class.forName("com.mysql.jdbc.Driver");
+
+		con = DriverManager.getConnection("jdbc:mysql://"
+			+ DataBaseINFO.MYSQL_DATABASE_SERVER, DataBaseINFO.MYSQL_USERNAME, DataBaseINFO.MYSQL_PASSWORD);
+
+		Statement stmt = con.createStatement();
+		stmt.executeQuery("USE " + DataBaseINFO.MYSQL_DATABASE_NAME);
+	}
+	
+	public Connection getConnection() {
+		return con;
+	}
+	
+	public void closeConnection() throws SQLException {
+		con.close();
+	}
+	
 }
