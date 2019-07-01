a13114c7e4cdcf01ab4da65a30e9ad34ac9f1de5
Alex <asurg17@freeuni.edu.ge>
2019-07-01 14:26:10 +0400

    Added message class

---
+++ WebQuiz/src/Temp/Message.java
@@ -0,0 +1,50 @@
+package Temp;
+
+import java.util.Date;
+
+public class Message {
+	
+	private Account from;
+	private Account to;
+	private String message;
+	private Date date;
+	
+	public Message(Account from, Account to, String message, Date date) {
+		this.from = from;
+		this.to = to;
+		this.message = message;
+		this.date = date;
+	}
+
+	public Account getFrom() {
+		return from;
+	}
+
+	public void setFrom(Account from) {
+		this.from = from;
+	}
+
+	public Account getTo() {
+		return to;
+	}
+
+	public void setTo(Account to) {
+		this.to = to;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+}
