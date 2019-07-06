40dd197d4089b8235e3113abf8f8cea7cf7eb28a
rlobz17 <rlobz17@freeuni.edu.ge>
2019-07-07 03:00:25 +0400

    Added QuizCategories class.

---
+++ WebQuiz/src/Temp/QuizCategories.java
@@ -0,0 +1,87 @@
+package Temp;
+
+public class QuizCategories {
+
+	
+	private static final String CATEGORY_CULTURE = "culture"; // კულტურა
+	private static final String CATEGORY_RELIGION = "religion"; // რელიგია
+	private static final String CATEGORY_POLITICS = "politics"; // პოლიტიკა
+	private static final String CATEGORY_SPORT = "sport"; // სპორტი
+	private static final String CATEGORY_HISTORY = "history"; // ისტორია
+	private static final String CATEGORY_SCIENCE = "science"; // მეცნიერება
+	private static final String CATEGORY_ART = "art"; // ხელოვნება
+	private static final String CATEGORY_KIDS = "kids"; // საბავშვო
+	private static final String CATEGORY_MEDICINE = "medicine"; // მედიცინა
+	private static final String CATEGORY_OTHERS = "others"; // სხვა
+	
+	/**
+	 * @return the categoryCulture
+	 */
+	public static String getCategoryCulture() {
+		return CATEGORY_CULTURE;
+	}
+
+	/**
+	 * @return the categoryReligion
+	 */
+	public static String getCategoryReligion() {
+		return CATEGORY_RELIGION;
+	}
+
+	/**
+	 * @return the categoryPolitics
+	 */
+	public static String getCategoryPolitics() {
+		return CATEGORY_POLITICS;
+	}
+
+	/**
+	 * @return the categorySport
+	 */
+	public static String getCategorySport() {
+		return CATEGORY_SPORT;
+	}
+
+	/**
+	 * @return the categoryHistory
+	 */
+	public static String getCategoryHistory() {
+		return CATEGORY_HISTORY;
+	}
+
+	/**
+	 * @return the categoryScience
+	 */
+	public static String getCategoryScience() {
+		return CATEGORY_SCIENCE;
+	}
+
+	/**
+	 * @return the categoryArt
+	 */
+	public static String getCategoryArt() {
+		return CATEGORY_ART;
+	}
+
+	/**
+	 * @return the categoryKids
+	 */
+	public static String getCategoryKids() {
+		return CATEGORY_KIDS;
+	}
+
+	/**
+	 * @return the categoryMedicine
+	 */
+	public static String getCategoryMedicine() {
+		return CATEGORY_MEDICINE;
+	}
+
+	/**
+	 * @return the categoryOthers
+	 */
+	public static String getCategoryOthers() {
+		return CATEGORY_OTHERS;
+	}
+	
+}
