aca0cb6c7e080f64de6c04e1cbff96e58a0d2be8
Shota <snoza17@freeuni.edu.ge>
2019-07-10 21:17:14 +0400

    add quiz servlet

---
+++ WebQuiz/src/Quiz/addQuizServlet.java
@@ -0,0 +1,41 @@
+package Quiz;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class addQuizServlet
+ */
+@WebServlet("/Quiz/addQuizServlet")
+public class addQuizServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public addQuizServlet() {
+        super();
+        // Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Auto-generated method stub
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Auto-generated method stub
+		
+	}
+
+}
