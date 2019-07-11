1352b0230c5b4d502208cf14427b159ca4bcb778
dudu <dpopk17@freeuni.edu.ge>
2019-07-12 00:57:22 +0400

    Question Creator uploaded, Tests uploaded, all test cases pass

--- WebQuiz/src/Question/QuestionCreator.java
+++ WebQuiz/src/Question/QuestionCreator.java
@@ -1,18 +1,80 @@
 package Question;
 
+import java.util.ArrayList;
+
+import Temp.Answer;
 import Temp.Question;
 
+
 public class QuestionCreator {
+
 	public Question createMatchingQuestionQ() {
 		return null;
 	}
-	public Question createMultipleChoiceQ() {
-		return null;
+	
+	/**
+	 * @param questionTypeID as int, which question type it is
+	 * @param questionDetail as string
+	 * @param imgUrl as string, if empty imgurl is set to null
+	 * @param possibleAnswersString as all given answers
+	 * @param correct answer as string, only one correct answer
+	 * @return Question type object with given data
+	 * */
+	public Question createMultipleChoiceQ(int questionTypeID,String questionDetail, String imgUrl, ArrayList<String> possibleAnswersString,String correctAnswer) {
+		ArrayList<Answer> possibleAnswers = new ArrayList<Answer>();
+		for (int i = 0; i <possibleAnswersString.size(); i++) {
+			possibleAnswers.add(new Answer(0, 0,i, possibleAnswersString.get(i)));
+		}
+		ArrayList<Answer> correctAnswers = new ArrayList<Answer>();
+		correctAnswers.add(new Answer(0, 0, 0, correctAnswer));
+		
+		if(imgUrl.isEmpty()) imgUrl = null;
+		
+		return new Question(0, questionTypeID, questionDetail, null, imgUrl, correctAnswers, possibleAnswers, false);
 	}
-	public Question createMultipleChoiceAndAnswerQ() {
-		return null;
+	
+	/**
+	 * @param questionTypeID as int, which question type it is
+	 * @param questionDetail as string
+	 * @param imgUrl as string, if empty imgurl is set to null
+	 * @param possibleAnswersString as all given answers
+	 * @param correct answers arraylist, filled with acceptable answers
+	 * @return Question type object with given data
+	 * */
+	public Question createMultipleChoiceAndAnswerQ(int questionTypeID,String questionDetail, String imgUrl, ArrayList<String> possibleAnswersString,ArrayList<String> correctAnswersString) {
+		ArrayList<Answer> possibleAnswers = new ArrayList<Answer>();	
+		for (int i = 0; i <possibleAnswersString.size(); i++) {
+			possibleAnswers.add(new Answer(0, 0,i, possibleAnswersString.get(i)));
+		}
+		
+		ArrayList<Answer> correctAnswers = new ArrayList<Answer>();
+		for (int i = 0; i <correctAnswersString.size(); i++) {
+			correctAnswers.add(new Answer(0, 0,i, correctAnswersString.get(i)));
+		}
+		
+		if(imgUrl.isEmpty()) imgUrl = null;
+		
+		return new Question(0, questionTypeID, questionDetail, null, imgUrl, correctAnswers, possibleAnswers, false);
 	}
-	public Question createMultiAnswerQ() {
-		return null;
+	
+	/**
+	 * @param questionTypeID as int, which question type it is
+	 * @param questionDetail as string
+	 * @param imgUrl as string, if empty imgurl is set to null
+	 * @param correct answers arraylist, filled with acceptable answers
+	 * @param bool order, represents if order is important for question
+	 * @return Question type object with given data
+	 * */
+	public Question createMultiAnswerQ(int questionTypeID,String questionDetail, String imgUrl,ArrayList<String> correctAnswersString, boolean order) {
+		ArrayList<Answer> possibleAnswers = new ArrayList<Answer>();	
+		
+		ArrayList<Answer> correctAnswers = new ArrayList<Answer>();
+		for (int i = 0; i <correctAnswersString.size(); i++) {
+			correctAnswers.add(new Answer(0, 0,i, correctAnswersString.get(i)));
+		}
+		
+		if(imgUrl.isEmpty()) imgUrl = null;
+		
+		return new Question(0, questionTypeID, questionDetail, null, imgUrl, correctAnswers, possibleAnswers, order);
 	}
 }
---
+++ WebQuiz/src/Question/QuestionCreatorTests.java
@@ -0,0 +1,136 @@
+package Question;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import Temp.Answer;
+import Temp.Question;
+
+public class QuestionCreatorTests {
+	private QuestionCreator questionCreator;
+	
+	@Before
+	public void before() {
+		questionCreator = new QuestionCreator();
+	}
+	
+	@Test
+	public void testMultipleChoiceCrator() {
+		int questionTypeID = 2;
+		String questionDetail = "Who am I?";
+		String imgUrl = "";
+		ArrayList<String> possibleAnswersString = new ArrayList<String>();
+		possibleAnswersString.add("I");
+		possibleAnswersString.add("Me");
+		possibleAnswersString.add("You");
+		possibleAnswersString.add("She");
+		
+		String correctAnswer = "You";
+		Question result = questionCreator.createMultipleChoiceQ(questionTypeID, questionDetail, imgUrl, possibleAnswersString, correctAnswer);
+		ArrayList<Answer> correctAnswers = result.getCorrectAnswers();
+		assertEquals(correctAnswer, correctAnswers.get(0).getAnswerDetail());
+		
+		ArrayList<Answer> possibleAnswers = result.getPossibleAnswers();
+		assertEquals(possibleAnswersString.get(0), possibleAnswers.get(0).getAnswerDetail());
+		assertEquals(possibleAnswersString.get(1), possibleAnswers.get(1).getAnswerDetail());
+		assertEquals(possibleAnswersString.get(2), possibleAnswers.get(2).getAnswerDetail());
+		assertEquals(possibleAnswersString.get(3), possibleAnswers.get(3).getAnswerDetail());
+		
+		assertEquals(null, result.getQuestionImgUrl());
+		assertEquals(2,result.getQuestionType());
+		assertEquals(false, result.getQuestionAnswerOrder());
+	}
+	
+	@Test
+	public void testMultipleChoiceAndAnswerCreator() {
+		int questionTypeID = 3;
+		String questionDetail = "mark all answers which are < 10";
+		String imgUrl = "";
+		ArrayList<String> possibleAnswersString = new ArrayList<String>();
+		possibleAnswersString.add("14");
+		possibleAnswersString.add("4");
+		possibleAnswersString.add("3");
+		possibleAnswersString.add("7");
+		possibleAnswersString.add("9");
+		possibleAnswersString.add("10");
+		possibleAnswersString.add("1");
+		possibleAnswersString.add("18");
+		
+		
+		ArrayList<String> correctAnswersString = new ArrayList<String>();
+		correctAnswersString.add("1");
+		correctAnswersString.add("3");
+		correctAnswersString.add("4");
+		correctAnswersString.add("7");
+		correctAnswersString.add("9");
+		
+		Question result = questionCreator.createMultipleChoiceAndAnswerQ(questionTypeID, questionDetail, imgUrl, possibleAnswersString, correctAnswersString);
+		ArrayList<Answer> correctAnswers = result.getCorrectAnswers();
+		
+		assertEquals(correctAnswersString.get(0), correctAnswers.get(0).getAnswerDetail());
+		assertEquals(correctAnswersString.get(1), correctAnswers.get(1).getAnswerDetail());
+		assertEquals(correctAnswersString.get(2), correctAnswers.get(2).getAnswerDetail());
+		assertEquals(correctAnswersString.get(3), correctAnswers.get(3).getAnswerDetail());
+		assertEquals(correctAnswersString.get(4), correctAnswers.get(4).getAnswerDetail());
+		
+		
+		
+		ArrayList<Answer> possibleAnswers = result.getPossibleAnswers();
+		assertEquals(possibleAnswersString.get(0), possibleAnswers.get(0).getAnswerDetail());
+		assertEquals(possibleAnswersString.get(1), possibleAnswers.get(1).getAnswerDetail());
+		assertEquals(possibleAnswersString.get(2), possibleAnswers.get(2).getAnswerDetail());
+		assertEquals(possibleAnswersString.get(3), possibleAnswers.get(3).getAnswerDetail());
+		assertEquals(possibleAnswersString.size(), possibleAnswers.size());
+		
+		assertEquals(false, result.getQuestionAnswerOrder());
+	}
+	
+	
+	@Test
+	public void testMultiAnswerCreator1() {
+		int questionTypeID = 1;
+		String questionDetail = "what is 5+5 and 10+10";
+		String imgUrl = "";
+		
+		ArrayList<String> correctAnswersString = new ArrayList<String>();
+		correctAnswersString.add("10");
+		correctAnswersString.add("20");
+
+		
+		Question result = questionCreator.createMultiAnswerQ(questionTypeID, questionDetail, imgUrl, correctAnswersString, true);
+		ArrayList<Answer> correctAnswers = result.getCorrectAnswers();
+		
+		assertEquals(correctAnswersString.get(0), correctAnswers.get(0).getAnswerDetail());
+		assertEquals(correctAnswersString.get(1), correctAnswers.get(1).getAnswerDetail());
+	
+		
+		assertEquals(true, result.getQuestionAnswerOrder());
+	}
+	
+	@Test
+	public void testMultiAnswerCreator2() {
+		int questionTypeID = 1;
+		String questionDetail = "what is 5+5 and 10+10";
+		String imgUrl = "";
+		
+		ArrayList<String> correctAnswersString = new ArrayList<String>();
+		correctAnswersString.add("10");
+		correctAnswersString.add("20");
+
+		
+		Question result = questionCreator.createMultiAnswerQ(questionTypeID, questionDetail, imgUrl, correctAnswersString, false);
+		ArrayList<Answer> correctAnswers = result.getCorrectAnswers();
+		
+		assertEquals(correctAnswersString.get(0), correctAnswers.get(0).getAnswerDetail());
+		assertEquals(correctAnswersString.get(1), correctAnswers.get(1).getAnswerDetail());
+	
+		
+		assertEquals(false, result.getQuestionAnswerOrder());
+	}
+	
+	
+}
